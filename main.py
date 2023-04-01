01355f43afde45dad493062f24342146b1cb5185
blitheli <blitheli@gmail.com>
2023-04-01 13:27:29 +0800

    修改了openai的调用接口为Completion

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ print('----openai------')
 openai.organization = "org-DaDfB9xHQ5ySB3qRdJC8G0zD"
 openai.api_key = os.getenv('OPENAI_API_KEY')
 openai.Model.list()
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo", 
-  messages=[{"role": "user", "content": "北京有多大？"}]
-)
 
-print(completion)
+# 调用openAi接口
+response = openai.Completion.create(model="text-davinci-003",
+                                    prompt="中国的陆地面积多大？",
+                                    max_tokens=100,
+                                    temperature=0.5)
+
+
+print("-------------GPT返回的消息----------------------")
+
+print(response.choices[0].text.strip())
